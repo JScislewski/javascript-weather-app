7d1f5cc7bfde2c04e901b9d3836d97c23f472f88
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-06-22 17:33:58 +0200

    Add DOM

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 window.addEventListener("load", () => {
   let lon;
   let lat;
+  let weatherMain = document.querySelector(".weather-main");
+  let temperatureDegree = document.querySelector(".temperature-degree");
   let temperatureDescription = document.querySelector(
     ".temperature-description"
   );
-  let temperatureDegree = document.querySelector(".temperature-degree");
-  let locationTimezone = document.querySelector(".location-timezone");
+  let weatherIcon = document.querySelector(".weather-icon");
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
       console.log(position);
@@ -17,8 +18,11 @@ window.addEventListener("load", () => {
         .then((response) => response.json())
         .then((data) => {
           const { temp } = data.main;
-          //const {}
+          const { main, description, icon } = data.weather[0];
+          weatherMain.textContent = main;
           temperatureDegree.textContent = temp;
+          temperatureDescription.textContent = description;
+          weatherIcon.src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
         });
     });
   } else {
